3d2ed0d70a3ede714a211b3eec5def2e351c2c03
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-05-08 12:06:46 +0200

    add Mail dadi completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,33 @@ btnReset.addEventListener('click',function(){
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
 // Stabilire il vincitore, in base a chi fa il punteggio più alto
 
+//creo le costanti per i bottoni play e stop
+const btnPlay = document.getElementById('play') 
+const btnStop = document.getElementById('stop') 
+
+btnPlay.addEventListener('click',function(){
+    
+    //creo le costanti per generare i numeri random
+    let myNut = Math.floor(Math.random()*6)+1;
+    document.getElementById('my_nut').innerHTML = `Tu hai fatto : ${myNut}`
+    let cpuNut = Math.floor(Math.random()*6)+1;
+    document.getElementById('cpu_nut').innerHTML = `La cpu ha fatto : ${cpuNut}`
+
+   
+
+    if (myNut < cpuNut) {
+       document.getElementById('result').innerHTML='Hai Perso!'
+    } else if (myNut === cpuNut){
+        document.getElementById('result').innerHTML='Hai Pareggiato!'
+    } else {
+        document.getElementById('result').innerHTML='Hai Vinto!'
+    }
+    
+}
+)
+
+btnStop.addEventListener('click',function(){
+    document.getElementById('result').innerHTML=''
+    document.getElementById('my_nut').innerHTML=''
+    document.getElementById('cpu_nut').innerHTML=''
+})
